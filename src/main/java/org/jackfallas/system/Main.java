9691334790d0334af3ed66bfc3c9d36dac187ffa
EduardoLG <jlopez-2023086@kinal.edu.gt>
2025-07-08 11:47:47 -0600

    Reestructuración Proyecto
    Se reestructuro el proyecto

--- src/main/java/org/jackfallas/system/Main.java
+++ src/main/java/org/jackfallas/system/Main.java
@@ -1,13 +1,28 @@
 package org.jackfallas.system;
-
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * @author informatica
  */
-public class Main {
+public class Main extends Application{
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        launch(args);
     }
+    
+    @Override
+    public void start(Stage escenario) throws Exception {
+        FXMLLoader cargador = new FXMLLoader(getClass().getResource("/view/ProductsDBView.fxml"));
+        Parent raiz = cargador.load();
+        Scene escena = new Scene(raiz);
+        escenario.setScene(escena);
+        escenario.show();
+    }
+    
 }
