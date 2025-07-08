de1dc57583d536b37f2dcf978e65c4a4e6921c30
Jack Fallas <jfallas-2021655@kinal.edu.gt>
2025-07-08 10:19:14 -0600

    Base de proyecto maven
    Subir archivos base del proyecto

---
+++ src/main/java/org/jackfallas/system/Main.java
@@ -0,0 +1,13 @@
+package org.jackfallas.system;
+
+
+/**
+ *
+ * @author informatica
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
